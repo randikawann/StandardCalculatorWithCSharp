9aad0726dbc6497fec491d2e0ec9bb274111c475
Randika wanninayaka <randikawann@gmail.com>
2017-10-16 00:04:27 -0400

    second commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,357 @@
+﻿namespace StandardCalculaorCShard
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.butDot = new System.Windows.Forms.Button();
+            this.but00 = new System.Windows.Forms.Button();
+            this.but0 = new System.Windows.Forms.Button();
+            this.but3 = new System.Windows.Forms.Button();
+            this.but2 = new System.Windows.Forms.Button();
+            this.but1 = new System.Windows.Forms.Button();
+            this.but6 = new System.Windows.Forms.Button();
+            this.but5 = new System.Windows.Forms.Button();
+            this.but4 = new System.Windows.Forms.Button();
+            this.but9 = new System.Windows.Forms.Button();
+            this.but8 = new System.Windows.Forms.Button();
+            this.but7 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.plBut = new System.Windows.Forms.Button();
+            this.eqBut = new System.Windows.Forms.Button();
+            this.divBut = new System.Windows.Forms.Button();
+            this.mulBut = new System.Windows.Forms.Button();
+            this.minBut = new System.Windows.Forms.Button();
+            this.cBut = new System.Windows.Forms.Button();
+            this.acBut = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(40, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(258, 85);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(4, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(251, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(202, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Standard Calculator";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.butDot);
+            this.panel2.Controls.Add(this.but00);
+            this.panel2.Controls.Add(this.but0);
+            this.panel2.Controls.Add(this.but3);
+            this.panel2.Controls.Add(this.but2);
+            this.panel2.Controls.Add(this.but1);
+            this.panel2.Controls.Add(this.but6);
+            this.panel2.Controls.Add(this.but5);
+            this.panel2.Controls.Add(this.but4);
+            this.panel2.Controls.Add(this.but9);
+            this.panel2.Controls.Add(this.but8);
+            this.panel2.Controls.Add(this.but7);
+            this.panel2.Location = new System.Drawing.Point(18, 127);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(196, 213);
+            this.panel2.TabIndex = 1;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
+            // 
+            // butDot
+            // 
+            this.butDot.Location = new System.Drawing.Point(139, 168);
+            this.butDot.Name = "butDot";
+            this.butDot.Size = new System.Drawing.Size(39, 23);
+            this.butDot.TabIndex = 11;
+            this.butDot.Text = ".";
+            this.butDot.UseVisualStyleBackColor = true;
+            this.butDot.Click += new System.EventHandler(this.butDot_Click);
+            // 
+            // but00
+            // 
+            this.but00.Location = new System.Drawing.Point(75, 168);
+            this.but00.Name = "but00";
+            this.but00.Size = new System.Drawing.Size(39, 23);
+            this.but00.TabIndex = 10;
+            this.but00.Text = "00";
+            this.but00.UseVisualStyleBackColor = true;
+            this.but00.Click += new System.EventHandler(this.but00_Click);
+            // 
+            // but0
+            // 
+            this.but0.Location = new System.Drawing.Point(13, 168);
+            this.but0.Name = "but0";
+            this.but0.Size = new System.Drawing.Size(39, 23);
+            this.but0.TabIndex = 9;
+            this.but0.Text = "0";
+            this.but0.UseVisualStyleBackColor = true;
+            this.but0.Click += new System.EventHandler(this.but0_Click);
+            // 
+            // but3
+            // 
+            this.but3.Location = new System.Drawing.Point(139, 118);
+            this.but3.Name = "but3";
+            this.but3.Size = new System.Drawing.Size(39, 23);
+            this.but3.TabIndex = 8;
+            this.but3.Text = "3";
+            this.but3.UseVisualStyleBackColor = true;
+            this.but3.Click += new System.EventHandler(this.but3_Click);
+            // 
+            // but2
+            // 
+            this.but2.Location = new System.Drawing.Point(75, 118);
+            this.but2.Name = "but2";
+            this.but2.Size = new System.Drawing.Size(39, 23);
+            this.but2.TabIndex = 7;
+            this.but2.Text = "2";
+            this.but2.UseVisualStyleBackColor = true;
+            this.but2.Click += new System.EventHandler(this.but2_Click);
+            // 
+            // but1
+            // 
+            this.but1.Location = new System.Drawing.Point(13, 118);
+            this.but1.Name = "but1";
+            this.but1.Size = new System.Drawing.Size(39, 23);
+            this.but1.TabIndex = 6;
+            this.but1.Text = "1";
+            this.but1.UseVisualStyleBackColor = true;
+            this.but1.Click += new System.EventHandler(this.but1_Click);
+            // 
+            // but6
+            // 
+            this.but6.Location = new System.Drawing.Point(139, 65);
+            this.but6.Name = "but6";
+            this.but6.Size = new System.Drawing.Size(39, 23);
+            this.but6.TabIndex = 5;
+            this.but6.Text = "6";
+            this.but6.UseVisualStyleBackColor = true;
+            this.but6.Click += new System.EventHandler(this.but6_Click);
+            // 
+            // but5
+            // 
+            this.but5.Location = new System.Drawing.Point(75, 65);
+            this.but5.Name = "but5";
+            this.but5.Size = new System.Drawing.Size(39, 23);
+            this.but5.TabIndex = 5;
+            this.but5.Text = "5";
+            this.but5.UseVisualStyleBackColor = true;
+            this.but5.Click += new System.EventHandler(this.but5_Click);
+            // 
+            // but4
+            // 
+            this.but4.Location = new System.Drawing.Point(13, 65);
+            this.but4.Name = "but4";
+            this.but4.Size = new System.Drawing.Size(39, 23);
+            this.but4.TabIndex = 3;
+            this.but4.Text = "4";
+            this.but4.UseVisualStyleBackColor = true;
+            this.but4.Click += new System.EventHandler(this.but4_Click);
+            // 
+            // but9
+            // 
+            this.but9.Location = new System.Drawing.Point(139, 15);
+            this.but9.Name = "but9";
+            this.but9.Size = new System.Drawing.Size(39, 23);
+            this.but9.TabIndex = 2;
+            this.but9.Text = "9";
+            this.but9.UseVisualStyleBackColor = true;
+            this.but9.Click += new System.EventHandler(this.but9_Click);
+            // 
+            // but8
+            // 
+            this.but8.Location = new System.Drawing.Point(75, 15);
+            this.but8.Name = "but8";
+            this.but8.Size = new System.Drawing.Size(39, 23);
+            this.but8.TabIndex = 1;
+            this.but8.Text = "8";
+            this.but8.UseVisualStyleBackColor = true;
+            this.but8.Click += new System.EventHandler(this.but8_Click);
+            // 
+            // but7
+            // 
+            this.but7.Location = new System.Drawing.Point(13, 15);
+            this.but7.Name = "but7";
+            this.but7.Size = new System.Drawing.Size(39, 23);
+            this.but7.TabIndex = 0;
+            this.but7.Text = "7";
+            this.but7.UseVisualStyleBackColor = true;
+            this.but7.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.plBut);
+            this.panel3.Controls.Add(this.eqBut);
+            this.panel3.Controls.Add(this.divBut);
+            this.panel3.Controls.Add(this.mulBut);
+            this.panel3.Controls.Add(this.minBut);
+            this.panel3.Controls.Add(this.cBut);
+            this.panel3.Controls.Add(this.acBut);
+            this.panel3.Location = new System.Drawing.Point(220, 105);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(109, 243);
+            this.panel3.TabIndex = 2;
+            // 
+            // plBut
+            // 
+            this.plBut.Location = new System.Drawing.Point(3, 140);
+            this.plBut.Name = "plBut";
+            this.plBut.Size = new System.Drawing.Size(44, 95);
+            this.plBut.TabIndex = 10;
+            this.plBut.Text = "+";
+            this.plBut.UseVisualStyleBackColor = true;
+            this.plBut.Click += new System.EventHandler(this.button17_Click);
+            // 
+            // eqBut
+            // 
+            this.eqBut.Location = new System.Drawing.Point(62, 197);
+            this.eqBut.Name = "eqBut";
+            this.eqBut.Size = new System.Drawing.Size(44, 38);
+            this.eqBut.TabIndex = 9;
+            this.eqBut.Text = "=";
+            this.eqBut.UseVisualStyleBackColor = true;
+            this.eqBut.Click += new System.EventHandler(this.eqBut_Click);
+            // 
+            // divBut
+            // 
+            this.divBut.Location = new System.Drawing.Point(62, 79);
+            this.divBut.Name = "divBut";
+            this.divBut.Size = new System.Drawing.Size(44, 38);
+            this.divBut.TabIndex = 9;
+            this.divBut.Text = "/";
+            this.divBut.UseVisualStyleBackColor = true;
+            this.divBut.Click += new System.EventHandler(this.divBut_Click);
+            // 
+            // mulBut
+            // 
+            this.mulBut.Location = new System.Drawing.Point(3, 79);
+            this.mulBut.Name = "mulBut";
+            this.mulBut.Size = new System.Drawing.Size(44, 38);
+            this.mulBut.TabIndex = 8;
+            this.mulBut.Text = "X";
+            this.mulBut.UseVisualStyleBackColor = true;
+            this.mulBut.Click += new System.EventHandler(this.mulBut_Click);
+            // 
+            // minBut
+            // 
+            this.minBut.Location = new System.Drawing.Point(62, 140);
+            this.minBut.Name = "minBut";
+            this.minBut.Size = new System.Drawing.Size(44, 38);
+            this.minBut.TabIndex = 7;
+            this.minBut.Text = "-";
+            this.minBut.UseVisualStyleBackColor = true;
+            this.minBut.Click += new System.EventHandler(this.minBut_Click);
+            // 
+            // cBut
+            // 
+            this.cBut.Location = new System.Drawing.Point(62, 22);
+            this.cBut.Name = "cBut";
+            this.cBut.Size = new System.Drawing.Size(44, 38);
+            this.cBut.TabIndex = 7;
+            this.cBut.Text = "C";
+            this.cBut.UseVisualStyleBackColor = true;
+            this.cBut.Click += new System.EventHandler(this.cBut_Click);
+            // 
+            // acBut
+            // 
+            this.acBut.Location = new System.Drawing.Point(3, 22);
+            this.acBut.Name = "acBut";
+            this.acBut.Size = new System.Drawing.Size(44, 38);
+            this.acBut.TabIndex = 6;
+            this.acBut.Text = "AC";
+            this.acBut.UseVisualStyleBackColor = true;
+            this.acBut.Click += new System.EventHandler(this.acBut_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(341, 360);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button but7;
+        private System.Windows.Forms.Button but9;
+        private System.Windows.Forms.Button but8;
+        private System.Windows.Forms.Button butDot;
+        private System.Windows.Forms.Button but00;
+        private System.Windows.Forms.Button but0;
+        private System.Windows.Forms.Button but3;
+        private System.Windows.Forms.Button but2;
+        private System.Windows.Forms.Button but1;
+        private System.Windows.Forms.Button but6;
+        private System.Windows.Forms.Button but5;
+        private System.Windows.Forms.Button but4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button cBut;
+        private System.Windows.Forms.Button acBut;
+        private System.Windows.Forms.Button plBut;
+        private System.Windows.Forms.Button divBut;
+        private System.Windows.Forms.Button mulBut;
+        private System.Windows.Forms.Button eqBut;
+        private System.Windows.Forms.Button minBut;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StandardCalculaorCShard
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        String s;
+        double beforeCal;
+        double afterCal;
+        String algo;
+        private void calButton(String algo1)
+        {
+            switch (algo1)
+            {
+                case "+":
+                    algo = "+";
+                    equal();
+                    textBox1.Text = beforeCal + "+";
+                    
+                    break;
+                case "X":
+                    algo = "X";
+                    equal();
+                    textBox1.Text = beforeCal + "X";
+                    break;
+                case "/":
+                    algo = "/";
+                    equal();
+                    textBox1.Text = beforeCal + "*";
+                    break;
+                case "-":
+                    algo = "-";
+                    equal();
+                    textBox1.Text = beforeCal + "-";
+                    break;
+                default:
+                    textBox1.Text = "0";
+                    break;
+            }
+
+            s = "";
+
+        }
+        double equal()
+        {
+            try
+            {
+                afterCal = Double.Parse(s);
+            }
+            catch (Exception e)
+            {
+                textBox1.Text = "E";
+            }
+            switch (algo)
+            {
+                case "+":
+                    beforeCal = beforeCal + afterCal;
+                    break;
+                case "X":
+                    if (beforeCal == 0)
+                        beforeCal = afterCal;
+                    else
+                        beforeCal = beforeCal * afterCal;
+                    break;
+                case "/":
+                    if (beforeCal == 0)
+                        beforeCal = afterCal;
+                    else if (afterCal == 0)
+                        textBox1.Text = "E";
+                    else
+                        beforeCal = beforeCal / afterCal;
+                    break;
+                case "-":
+                    if (beforeCal == 0)
+                        beforeCal = afterCal;
+                    else
+                        beforeCal = beforeCal - afterCal;
+                    break;
+                default:
+                    textBox1.Text= "0";
+                    break;
+            }
+            return beforeCal;
+        }
+        void equalLast()
+        {
+            textBox1.Text = beforeCal.ToString();
+            beforeCal = 0;
+            s = "";
+        }
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = s + "7";
+            s = s + 7;
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            calButton("+");
+        }
+
+        private void but0_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = s + "0";
+            s = s + 0;
+        }
+
+        private void but00_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = s + "00";
+            s = s + 00;
+        }
+
+        private void butDot_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = s + ".";
+            s = s + ".";
+        }
+
+        private void but1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = s+"1";
+            s = s + 1;
+        }
+
+        private void but2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = s + "2";
+            s = s + 2;
+        }
+
+        private void but3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = s + "3";
+            s = s + 3;
+        }
+
+        private void but4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = s + "4";
+            s = s + 4;
+        }
+
+        private void but5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = s + "5";
+            s = s + 5;
+        }
+
+        private void but6_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = s + "6";
+            s = s + 6;
+        }
+
+        private void but8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = s + "8";
+            s = s + 8;
+        }
+
+        private void but9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = s + "9";
+            s = s + 9;
+        }
+
+        private void minBut_Click(object sender, EventArgs e)
+        {
+            calButton("-");
+        }
+
+        private void mulBut_Click(object sender, EventArgs e)
+        {
+            calButton("X");
+        }
+
+        private void divBut_Click(object sender, EventArgs e)
+        {
+            calButton("/");
+        }
+
+        private void acBut_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            s = "";
+            beforeCal = 0;
+        }
+
+        private void cBut_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            s = "";
+        }
+
+        private void eqBut_Click(object sender, EventArgs e)
+        {
+            equal();
+            equalLast();
+        }
+    }
+}
